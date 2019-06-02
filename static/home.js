a535f5ee6cf12a1351f2e334d4099135d06f8776
Vijayendra Pagare <vijayendrapagare05@gmail.com>
2019-06-02 22:15:32 +0530

    version 0.2

--- static/home.js
+++ static/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {
-	var socket = io.connect("http://localhost:3000");
+	var socket = io.connect("https://testchaatapp.herokuapp.com");
 	var message,chatMessages;
 
 	var username = $("#username").html();
